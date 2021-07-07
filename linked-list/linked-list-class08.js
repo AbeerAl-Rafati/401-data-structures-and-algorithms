7ab710518620221efd223cbf1bbdfc02bc7d2a04
AbeerAl-Rafati <Abeerw.rafati@yahoo.com>
2021-07-07 04:30:08 +0300

    ziplist

---
+++ linked-list/linked-list-class08.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Node = require('./node');
+
+
+class SinglyLinkedList08 {
+  constructor() {
+    this.head = null;
+
+  }
+
+
+  insert(value) {
+
+    this.head = new Node(value, this.head);
+
+  }
+
+  zip(list1, list2) {
+
+    let list3 = new Node('')
+    let tail = list3
+    let prv
+
+
+    while (list1 || list2) {
+      if (list1 !== null) tail.val += list1.val
+      if (list2 !== null) tail.val += list2.val
+
+      tail.next = new Node('')
+      prv = tail
+      tail = tail.next
+
+      list1 == list1 ? list1.next : list1
+      list2 == list2 ? list2.next : list2
+    }
+
+  }
+
+}
+
+module.exports = SinglyLinkedList08;
