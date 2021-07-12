446b75d4dd27b37276a157bc45c3d2419cf70f72
Abeer Al-Rafati <79833013+AbeerAl-Rafati@users.noreply.github.com>
2021-07-12 08:29:42 +0300

    11 manullay upload

---
+++ stack-and-queue/pseudo-queue.js
@@ -0,0 +1,40 @@
+const Stack = require('./stack');
+
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+
+  }
+
+  enqueue(value) {
+    this.stack1.push(value);
+
+  }
+
+
+  dequeue() {
+    while (this.stack1.head !== null) {
+      let removedItem = this.stack1.pop(); //remove last in 
+      this.stack2.push(removedItem);
+      // this.stack1.head = this.stack1.head.next;
+    }
+    return this.stack2.pop();
+  }
+
+}
+
+module.exports = PseudoQueue;
+
+// let qu1 = new PseudoQueue();
+// qu1.enqueue(1)
+// qu1.enqueue(2)
+// qu1.enqueue(3)
+
+// console.log(qu1);
+// console.log(qu1.head);
+
+
+// qu1.dequeue() // remove 3 , add it to stack2 { 3 }
+// qu1.dequeue() // remove 2 , add it to stack2 { 2 ,3  }
+// qu1.dequeue() // remove 1 , add it to stack2 { 1 ,2 ,3  }
